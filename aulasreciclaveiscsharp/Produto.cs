edc370bc78c576148423acd7f675331e4e0fb68b
Pqmeu <rdesouzamatheus@gmail.com>
2023-01-12 11:56:19 -0300

    primeiro teste de commits para projetos em c#(projeto de aula do curso do nélio alves)

---
+++ aulasreciclaveiscsharp/Produto.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace aulasreciclaveiscsharp
+{
+    internal class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto()
+        {
+            _nome = "produto";
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double Preco
+        {
+            get { return _preco;  }
+            set
+            {
+                if (value != 0)
+                {
+                    _preco = value;
+                }
+            }
+        }
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+
+        public double Total()
+        {
+            return _preco * _quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome + ", $" + _preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + _quantidade + " Unidades em estoque, " + "Preço total: $" + Total();
+        }
+    }
+}
---
+++ aulasreciclaveiscsharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace aulasreciclaveiscsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+
+            Console.WriteLine(produto);
+
+            Console.WriteLine("Entre com os dados do produto");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine());
+           
+            produto.Nome = nome;
+            produto.Preco = preco;
+
+            Console.WriteLine(produto);
+
+            Console.Write("Digite o total de produtos a serem adicionados ao estoque: ");
+
+            int qte = int.Parse(Console.ReadLine());
+            produto.AdicionarProdutos(qte);
+            Console.WriteLine(produto);
+
+            Console.Write("Digite o total de produtos a serem removidos do estoque: ");
+
+            qte = int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(qte);
+            Console.WriteLine(produto);
+        }
+    }
+}
